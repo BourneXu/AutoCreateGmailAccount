9c6234c650f1ceab21cc7288b6982d0bca6c04e7
Chao <chaoxu18@vt.edu>
2019-11-19 01:12:36 -0500

    Update: use Chrome driver

--- AutoCreateGmailAccount.py
+++ AutoCreateGmailAccount.py
@@ -14,56 +14,72 @@ import sys
 
 class CreateGmail:
     """Auto Create Gmail Accounts with popular names"""
+
     def __init__(self, firstname, lastname, username, pswd):
         self._firstname = firstname
         self._lastname = lastname
         self._username = username
         self._pswd = pswd
-        self._Donefile = open('/Users/X_Bourne/GoogleDrive/ecrime_phishing/Chaoxu/Data/GmailAccounts/GmailAccounts.csv', 'a')
+        self._Donefile = open("./data/CreatedAccounts.csv", "a")
         self.Initialize()
 
     def Initialize(self):
-        self._browser = webdriver.Firefox()
+        self._browser = webdriver.Chrome()
         self._browser.delete_all_cookies()
-        self._browser.get('https://accounts.google.com/SignUp?hl=en')
+        self._browser.get("https://accounts.google.com/SignUp?hl=en")
 
     def SetRecoveryEmail(self):
-        CreatedEmails = pd.read_csv('./data/CreatedAccounts.csv')['username'].values
+        CreatedEmails = pd.read_csv("./data/CreatedAccounts.csv")["username"].values
         if len(CreatedEmails) < 1:
-            self.recovery_email = 'pj.cs.vt@gmail.com'
+            self.recovery_email = "pj.cs.vt@gmail.com"
         else:
-            self.recovery_email = CreatedEmails[-1] + '@gmail.com'
-    
+            self.recovery_email = CreatedEmails[-1] + "@gmail.com"
+
     def CreateAccount(self):
         # self.SetRecoveryEmail()
-        self._browser.find_element_by_css_selector(r'input[id="firstName"]').send_keys(self._firstname)
+        self._browser.find_element_by_css_selector(r'input[id="firstName"]').send_keys(
+            self._firstname
+        )
         time.sleep(1)
-        self._browser.find_element_by_css_selector(r'input[id="lastName"]').send_keys(self._lastname)
+        self._browser.find_element_by_css_selector(r'input[id="lastName"]').send_keys(
+            self._lastname
+        )
         time.sleep(1)
-        self._browser.find_element_by_css_selector(r'input[id="username"]').send_keys(self._username)
+        self._browser.find_element_by_css_selector(r'input[id="username"]').send_keys(
+            self._username
+        )
         time.sleep(1)
         self._browser.find_element_by_css_selector(r'input[name="Passwd"]').send_keys(self._pswd)
         time.sleep(1 + 3 * random.random())
-        self._browser.find_element_by_css_selector(r'input[name="ConfirmPasswd"]').send_keys(self._pswd)
+        self._browser.find_element_by_css_selector(r'input[name="ConfirmPasswd"]').send_keys(
+            self._pswd
+        )
         self._browser.find_element_by_css_selector(r'div[id="accountDetailsNext"]').click()
         self._browser.implicitly_wait(10)
 
         try:
-            self._browser.find_element_by_css_selector('#month > option:nth-child(%d)'%random.randint(1, 13)).click()
+            self._browser.find_element_by_css_selector(
+                "#month > option:nth-child(%d)" % random.randint(1, 13)
+            ).click()
         except:
             self._browser.quit()
-            raise ValueError('IP Mac Limited. Stop the Script...')
-            sys.exit(0)
+            # raise ValueError("IP Mac Limited. Stop the Script...")
+            # sys.exit(0)
+            sys.exit("IP Mac Limited. Stop the Script...")
         else:
             time.sleep(1 + 3 * random.random())
             # self._browser.find_element_by_css_selector(r'div.fQxwff:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)').send_keys(self.recovery_email)
             # time.sleep(1)
-            self._browser.find_element_by_css_selector(r'input[id="day"]').send_keys(random.randint(1, 28))
+            self._browser.find_element_by_css_selector(r'input[id="day"]').send_keys(
+                random.randint(1, 28)
+            )
             time.sleep(1)
-            self._browser.find_element_by_css_selector(r'input[id="year"]').send_keys(random.randint(1990, 2000))
+            self._browser.find_element_by_css_selector(r'input[id="year"]').send_keys(
+                random.randint(1990, 2000)
+            )
             time.sleep(1)
             try:
-                self._browser.find_element_by_css_selector('#gender').click()
+                self._browser.find_element_by_css_selector("#gender").click()
             except:
                 print("Cannot locate Gender Blockm Please manually click it. Sleep 1 min...")
                 time.sleep(60)
@@ -71,30 +87,35 @@ class CreateGmail:
             try:
                 time.sleep(0.5)
                 try:
-                    self._browser.find_element_by_css_selector('#gender > option:nth-child(%d)'%random.randint(1, 4)).click()
+                    self._browser.find_element_by_css_selector(
+                        "#gender > option:nth-child(%d)" % random.randint(1, 4)
+                    ).click()
                 except:
                     self._browser.quit()
                     sys.exit(1)
-                    self._browser.find_elements_by_xpath('').click()
+                    self._browser.find_elements_by_xpath("").click()
             except:
                 time.sleep(0.5)
-                self._browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div[2]/form/div[2]/div/div[4]/div[1]/div/div[2]/select/option[%d]'%random.randint(1, 4)).click()
+                self._browser.find_element_by_xpath(
+                    "/html/body/div[1]/div/div[2]/div[1]/div[2]/form/div[2]/div/div[4]/div[1]/div/div[2]/select/option[%d]"
+                    % random.randint(1, 4)
+                ).click()
             time.sleep(1 + 3 * random.random())
             self._browser.find_element_by_css_selector(r'div[id="personalDetailsNext"]').click()
             self._browser.implicitly_wait(10)
             time.sleep(1)
             while True:
                 try:
-                    self._browser.find_element_by_css_selector('.mUbCce').click()
+                    self._browser.find_element_by_css_selector(".mUbCce").click()
                     time.sleep(1)
                 except Exception as e:
                     print(e)
                     break
-            self._browser.find_element_by_css_selector('#termsofserviceNext').click()
+            self._browser.find_element_by_css_selector("#termsofserviceNext").click()
             self._browser.implicitly_wait(10)
 
-            self._Donefile.write(self._username + ',' + self._pswd + '\n')
-            print(self._username + ': Success')
+            self._Donefile.write(self._username + "," + self._pswd + "\n")
+            print(self._username + ": Success")
             self._browser.quit()
 
     @staticmethod
@@ -104,43 +125,37 @@ class CreateGmail:
         num = min(len(FirstName), len(LastName))
         if len(FirstName) > len(LastName):
             UserInfo = LastName
-            UserInfo['firstname'] = FirstName.values[:num]
+            UserInfo["firstname"] = FirstName.values[:num]
         else:
             UserInfo = FirstName
-            UserInfo['lastname'] = LastName.values[:num]
+            UserInfo["lastname"] = LastName.values[:num]
         UserInfo.index = range(num)
         UserInfo.dropna()
-        suffix = ''
+        suffix = ""
         for i in range(6):
             suffix += str(random.randint(0, 9))
-        UserInfo['username'] = UserInfo['firstname'] + UserInfo['lastname'] + suffix
-        UserInfo['pswd'] = 'super' + UserInfo['firstname'] + '233'
+        UserInfo["username"] = UserInfo["firstname"] + UserInfo["lastname"] + suffix
+        UserInfo["pswd"] = "super" + UserInfo["firstname"] + "233"
         return UserInfo
 
-
-
     def RunAppsScript(self, sharedlink):
-        '''So far, cannot auto totally
+        """So far, cannot auto totally
         
         Open sharedlink and then, plz manually finish Install. 
-        '''
+        """
         self._browser.get(sharedlink)
         time.sleep(10)
 
 
+if __name__ == "__main__":
+    SharedScript = "https://script.google.com/d/1yihwFAHrV17XHYmnrOJxQasqWGourSD57Xi-oFYO3sgY-B1_inPt5Vkc/edit?usp=sharing"
 
-if __name__ == '__main__':
-    SharedScript = 'https://script.google.com/d/1yihwFAHrV17XHYmnrOJxQasqWGourSD57Xi-oFYO3sgY-B1_inPt5Vkc/edit?usp=sharing'
-
-    firstnamefile = './data/CSV_Database_of_First_Names.csv'
-    lastnamefile = './data/CSV_Database_of_Last_Names.csv'
+    firstnamefile = "./data/CSV_Database_of_First_Names.csv"
+    lastnamefile = "./data/CSV_Database_of_Last_Names.csv"
     UserInfoDF = CreateGmail.GetUserInfo(firstnamefile, lastnamefile)
     for num in range(len(UserInfoDF)):
         UserInfoSeries = UserInfoDF.loc[num]
         CGM = CreateGmail(*UserInfoSeries)
         CGM.CreateAccount()
         # CGM.RunAppsScript(SharedScript)
-
-    
-
 
--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 ### Preparation
 
 1. Python 3.x
-2. `pip install selenium, pandas`
-3. Download [Firefox](https://www.mozilla.org/en-US/firefox/new/) 
-4. Download [geckodriver](https://github.com/mozilla/geckodriver/releases)
-5. Copy geckodriver in `/usr/local/bin`
+2. `pip install -r requirements.txt`
+3. Download [Chrome](https://www.google.com/chrome/) 
+4. Download [chromedriver](https://chromedriver.chromium.org/downloads)
+5. Copy chromedriver in `/usr/local/bin`
 
 ### Run 
 
-Run AutoCreateGmailAccount.py
+`python AutoCreateGmailAccount.py`
 
 - **Step 1**
 
@@ -24,29 +24,11 @@ Run AutoCreateGmailAccount.py
 
   ![step2](./figures/step2.png)
 
-- **Step 3**
-
-  Following things are all automatic.
-
-  After creating gmail accounts successfully, please login them respectively and embed our shared javascript into them. Refer to https://bitbucket.org/gianluca_students/gmail-honeypot
-
-  1. Visit  [gas.js](https://script.google.com/d/1yihwFAHrV17XHYmnrOJxQasqWGourSD57Xi-oFYO3sgY-B1_inPt5Vkc/edit?usp=sharing) or [track.js](https://script.google.com/d/19cYjAJs5H9Z-WpCeNa6NF4E2usEXoyafAxzZ9IJ30rtMYsqId7fUSWSp/edit?usp=sharing) or [track2.js](https://script.google.com/d/1xQLzecX7dJ5PnnpOsJRDLkeBTWbj2fx7HytKPPqNZgJxJB-98qupMCTQ/edit?usp=sharing)
-
-     ![step3](./figures/step3.png)
-
-  2. Select function: **Install** 
-
-  3. Run it 
-
-     ![step4](./figures/step4.png)
-
-  4. Allow the permisson
 
 ### Lastly
 
 One person or laptop could create about 2 new accounts during about 2-3days.
 
-Please Send me /data/CreatedAccounts.csv after your finishing all above things.
 
 
 
